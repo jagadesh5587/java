01c4b7d904688fe9cb5f864c51fc8de0a55b630a
unknown <jagadesh.rudra@gmail.com>
2020-08-08 14:24:50 +0530

    second

---
+++ odd numbers/src/odd/OddNum.java
@@ -0,0 +1,14 @@
+package odd;
+
+public class OddNum {
+public static void main(String[] args) {
+	System.out.println("odd numbers are:");
+	for (int i = 0; i<=100; i++) {
+		if (i%2!=0)
+		System.out.println(i);
+			
+		
+		
+	}
+}
+}
